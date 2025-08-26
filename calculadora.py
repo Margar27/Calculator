0f349b417de56c52206b921e0495264354120238
Margar27 <martingarcif@gmail.com>
2025-08-26 20:45:27 +0200

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+n1= int(input("Primer numero"))
+n2= int (input("Segundo numero"))
+Op = input("Que quieres hacer? ")
+if Op == "suma":
+    print(n1 +n2)
+elif Op == "resta":
+    print(n1-n2)
+elif Op == "multiplicaión":
+    print(n1*n2)
+elif Op == "división":
+    print(n1/n2)
